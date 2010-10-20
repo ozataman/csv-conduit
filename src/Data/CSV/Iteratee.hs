7e8aa9cc4b9ec2a4e3bd609856e8a42dc14eec6d
Ozgun Ataman <ozataman@gmail.com>
2010-10-19 20:52:09 -0400

    * back to using attoparsec-enum

--- src/Data/CSV/Iteratee.hs
+++ src/Data/CSV/Iteratee.hs
@@ -43,7 +43,7 @@ import System.IO
 
 import Data.Attoparsec as P
 import qualified Data.Attoparsec.Char8 as C8
-import Data.Attoparsec.Enumerator
+import Data.Attoparsec.Enum
 import qualified Data.Enumerator as E
 import Data.Enumerator (($$))
 import Data.Enumerator.IO (enumFile)
