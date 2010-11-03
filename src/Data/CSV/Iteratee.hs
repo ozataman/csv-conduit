c650dd423260b82a064018dd064763aa48afb95c
Ozgun Ataman <ozataman@gmail.com>
2010-11-03 13:10:39 -0400

    * 0.5.0: Further generalize under the CSVeable class to unify folding over Row and MapRow based CSVs. Also add read/write functions.

--- src/Data/CSV/Iteratee.hs
+++ src/Data/CSV/Iteratee.hs
@@ -9,32 +9,40 @@ module Data.CSV.Iteratee
   , Field   -- Simply @ByteString@
   , MapRow  
 
+  , CSVeable
+  , rowToStr
+  , foldCSVFile
+
+  , ParsedRow(..) 
+
   -- * CSV Setttings
   , CSVSettings(..)
   , defCSVSettings
 
+  -- * Reading / Writing CSV Files
+  , readCSVFile
+  , writeCSVFile
+
   -- * Mapping Over CSV Files
+  -- | These functions take a CSV file, do something with each row and output the resulting rows as a new CSV file.
   , mapCSVFile
   , mapCSVMapFile
 
   -- * Folding Over CSV Files 
-  , foldCSVMapFile
-  , foldCSVFile 
+  -- | These enumerators generalize the map* family of functions with a running accumulator.
   , CSVAction
+  , funToIter
+  , funToIterIO
 
   -- * Primitive Iteratees
   , collectRows
-  
-  -- * Output Functions
-  , CSVeable
-  , rowToStr
   )
 
 where
 
 import Control.Applicative hiding (many)
 import Control.Exception (bracket, SomeException)
-import Control.Monad (mzero, mplus)
+import Control.Monad (mzero, mplus, foldM)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as B
 import Data.ByteString.Internal (c2w)
@@ -78,19 +86,75 @@ type Row = [Field]
 type Field = B.ByteString
 type MapRow = M.Map B.ByteString B.ByteString
 
-class CSVeable c where
-  rowToStr :: CSVSettings -> c -> B.ByteString
+class CSVeable r where
+
+  -- | Convert a CSV row into strict ByteString equivalent.
+  rowToStr :: CSVSettings -> r -> B.ByteString
+
+  -- | Open & fold over the CSV file. 
+  -- Processing starts on row 2 for MapRow instance to use first row as column
+  -- headers.
+  foldCSVFile :: FilePath -- ^ File to open as a CSV file
+              -> CSVSettings -- ^ CSV settings to use on the input file
+              -> CSVAction r a -- ^ Fold action
+              -> a  -- ^ Initial accumulator
+              -> IO (Either SomeException a) -- ^ Error or the resulting accumulator
 
 instance CSVeable Row where
   rowToStr s r = let sep = B.pack [c2w (csvOutputColSep s)] 
                      qt = c2w (csvOutputQuoteChar s)
                      wrapField f = qt `B.cons` f `B.snoc` qt
                  in B.intercalate sep . map wrapField $ r
+  
+  foldCSVFile fp csvs f acc = E.run (enumFile fp $$ loop acc)
+    where
+      loop !acc' = do
+        eof <- E.isEOF 
+        case eof of
+          True -> f acc' EOF
+          False -> comboIter acc'
+      comboIter acc' = procRow acc' >>= loop
+      procRow acc' = rowParser csvs >>= f acc' . ParsedRow
 
 instance CSVeable MapRow where
   rowToStr s r = rowToStr s . M.elems $ r
 
--- | Take a CSV file, apply function to each of its rows and save the resulting rows into a new file.
+  foldCSVFile fp csvs f !acc = E.run (enumFile fp $$ loop [] acc)
+    where
+      loop !headers !acc' = do
+        eof <- E.isEOF 
+        case eof of
+          True -> f acc' EOF
+          False -> comboIter headers acc'
+      comboIter headers acc' = procRow headers acc' >>= loop headers
+
+      -- Fill headers if not yet filled
+      procRow [] acc' = rowParser csvs >>= (\(Just headers) -> loop headers acc') 
+
+      -- Process starting w/ the second row
+      procRow headers acc' = rowParser csvs >>= toMapCSV headers >>= f acc' . ParsedRow 
+      toMapCSV headers fs = E.yield (fs >>= (Just . M.fromList . zip headers)) (E.Chunks [])
+
+
+readCSVFile :: (CSVeable r) => CSVSettings  -- ^ CSV settings
+            -> FilePath   -- ^ FilePath
+            -> IO (Either SomeException [r])  -- ^ Collected data
+readCSVFile s fp = foldCSVFile fp s collectRows []
+
+writeCSVFile :: (CSVeable r) => CSVSettings   -- ^ CSV settings
+             -> FilePath  -- ^ Target file path
+             -> [r]   -- ^ Data to be output
+             -> IO Int  -- ^ Number of rows written
+writeCSVFile s fp rs = 
+  let outputRows h = foldM (step h) 0  . map (rowToStr s) $ rs
+      step h acc x = (B.hPutStrLn h x) >> return (acc+1)
+  in bracket
+      (openFile fp WriteMode)
+      (hClose)
+      (outputRows)
+
+-- | Take a CSV file, 
+-- apply function to each of its rows and save the resulting rows into a new file.
 --
 -- Each row is simply a list of fields.
 mapCSVFile
@@ -124,7 +188,7 @@ mapCSVMapFile
   -> FilePath                         -- ^ Output file
   -> IO (Either SomeException Int)    -- ^ Number of rows processed 
 mapCSVMapFile fi s f fo = do
-  res <- foldCSVMapFile fi s mapIter (Nothing, 0)
+  res <- foldCSVFile fi s mapIter (Nothing, 0)
   return $ snd `fmap` res
   where
     mapIter :: (Maybe Handle, Int) -> ParsedRow MapRow -> E.Iteratee B.ByteString IO (Maybe Handle, Int)
@@ -147,56 +211,39 @@ mapCSVMapFile fi s f fo = do
 outputRow :: CSVeable r => CSVSettings -> Handle -> [r] -> E.Iteratee B.ByteString IO ()
 outputRow s oh = liftIO . mapM_ (B.hPutStrLn oh) . map (rowToStr s)
 
--- | A datatype to signal parsing status to the user-developed iteratee
+-- | A datatype that incorporates the signaling of parsing status to the user-developed iteratee.
+--
 -- We need this because some iteratees do interleaved IO (such as outputting to a file via a handle inside the accumulator)
--- And some final actions may need to be taken upon encountering EOF (such as closing the handle)
+-- and some final actions may need to be taken upon encountering EOF (such as closing the interleaved handle).
+--
+-- Use this datatype when developing iteratees for use with fold* family of functions (Row enumarators).
 data (CSVeable r) => ParsedRow r = ParsedRow (Maybe r) | EOF
 
 -- | An iteratee that processes each row of a CSV file and updates the accumulator.
 --
--- You would implement one of these to use with the 'foldCSVFile' or 'foldCSVMapFile' functions
+-- You would implement one of these to use with the 'foldCSVFile' function.
 type CSVAction r a = a -> ParsedRow r -> E.Iteratee B.ByteString IO a
 
--- | Open & fold over the CSV file. Processing starts on row 2 and each row is represented as a Map
--- using first row as column headers.
-foldCSVMapFile
-  :: FilePath -- ^ File to open as a CSV file
-  -> CSVSettings
-  -> CSVAction MapRow a -- ^ An iteratee that processes each row of a CSV file and updates the accumulator
-  -> a  -- ^ Initial accumulator
-  -> IO (Either SomeException a) -- ^ Error or the resulting accumulator
-foldCSVMapFile fp csvs f !acc = E.run (enumFile fp $$ loop [] acc)
-  where
-    loop !headers !acc' = do
-      eof <- E.isEOF 
-      case eof of
-        True -> f acc' EOF
-        False -> comboIter headers acc'
-    comboIter headers acc' = procRow headers acc' >>= loop headers
-    procRow [] acc' = rowParser csvs >>= (\(Just headers) -> loop headers acc') -- Fill headers if not yet filled
-    procRow headers acc' = rowParser csvs >>= toMapCSV headers >>= f acc' . ParsedRow -- Process starting w/ the second row
-    toMapCSV headers fs = E.yield (fs >>= (Just . M.fromList . zip headers)) (E.Chunks [])
-
-
--- | Same as 'foldCSVMapFile' but treats each row as @[ByteString]@.
+-- | Convenience converter for fold step functions that live in the IO monad.
 --
--- Processing starts on row 1, as there is no need for column names in this variant.
-foldCSVFile
-  :: FilePath -- ^ File to open as a CSV file
-  -> CSVSettings
-  -> CSVAction Row a -- ^ An iteratee that processes each row of a CSV file and updates the accumulator
-  -> a  -- ^ Initial accumulator
-  -> IO (Either SomeException a) -- ^ Error or the resulting accumulator
-foldCSVFile fp csvs f acc = E.run (enumFile fp $$ loop acc)
+-- Use this if you don't want to deal with Iteratees when writing your fold
+-- functions.
+funToIterIO :: (CSVeable r) => (a -> ParsedRow r -> IO a) -> CSVAction r a
+funToIterIO f = iterf
   where
-    loop !acc' = do
-      eof <- E.isEOF 
-      case eof of
-        True -> f acc' EOF
-        False -> comboIter acc'
-    comboIter acc' = procRow acc' >>= loop
-    procRow acc' = rowParser csvs >>= f acc' . ParsedRow
+    iterf !acc EOF = liftIO (f acc EOF) >>= \(!acc') -> E.yield acc' E.EOF
+    iterf !acc r = liftIO (f acc r) >>= \(!acc') -> E.yield acc' (E.Chunks [])
 
+
+-- | Convenience converter for fold step functions that are pure.
+--
+-- Use this if you don't want to deal with Iteratees when writing your fold
+-- functions.
+funToIter :: (CSVeable r) => (a -> ParsedRow r -> a) -> CSVAction r a
+funToIter f = iterf
+  where
+    iterf !acc EOF = E.yield (f acc EOF) E.EOF
+    iterf !acc r = E.yield (f acc r) (E.Chunks [])
 
 -- | Just collect all rows into an array. This will cancel out the incremental nature of this library.
 collectRows :: CSVeable r => CSVAction r [r]
@@ -212,7 +259,8 @@ row :: CSVSettings -> Parser (Maybe Row)
 row csvs = csvrow csvs <|> badrow
 
 badrow :: Parser (Maybe Row)
-badrow = P.takeWhile (not . C8.isEndOfLine) *> (C8.endOfLine <|> C8.endOfInput) *> return Nothing
+badrow = P.takeWhile (not . C8.isEndOfLine) *> 
+         (C8.endOfLine <|> C8.endOfInput) *> return Nothing
 
 csvrow :: CSVSettings -> Parser (Maybe Row)
 csvrow c = 
