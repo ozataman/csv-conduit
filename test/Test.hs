ea6d5ae3dc976c0bcdfee467c9c5dc0a0532d6c8
Ozgun Ataman <ozataman@gmail.com>
2012-10-07 17:49:12 -0400

    Testsuite tweaks

--- test/Test.hs
+++ test/Test.hs
@@ -1,24 +1,26 @@
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import qualified Data.ByteString.Char8 as B
-import Data.Map ((!))
-import Data.Text
-import System.Directory
-import Test.Framework (Test, defaultMain, testGroup)
-import Test.Framework.Providers.HUnit
-import Test.HUnit ((@=?))
+import qualified Data.ByteString.Char8          as B
+import           Data.Map                       ((!))
+import           Data.Text
+import           System.Directory
+import           Test.Framework                 (Test, defaultMain, testGroup)
+import           Test.Framework.Providers.HUnit
+import           Test.HUnit                     ((@=?))
 
-import Data.CSV.Conduit
+import           Data.CSV.Conduit
 
 
 main :: IO ()
 main = defaultMain tests
 
+
 tests :: [Test]
 tests = [testGroup "Basic Ops" baseTests]
+
 
 baseTests :: [Test]
 baseTests =
@@ -26,10 +28,11 @@ baseTests =
   , testCase "simple parsing works" test_simpleParse
   ]
 
+
 test_identityMap :: IO ()
 test_identityMap = do
-    _ <- runResourceT $ mapCSVFile csvSettings f testFile outFile
-    f1 <- readFile testFile
+    _ <- runResourceT $ mapCSVFile csvSettings f testFile2 outFile
+    f1 <- readFile testFile2
     f2 <- readFile outFile
     f1 @=? f2
     removeFile outFile
@@ -38,9 +41,10 @@ test_identityMap = do
     f :: Row Text -> [Row Text]
     f = return
 
+
 test_simpleParse :: IO ()
 test_simpleParse = do
-  (d :: [MapRow B.ByteString]) <- readCSVFile csvSettings testFile
+  (d :: [MapRow B.ByteString]) <- readCSVFile csvSettings testFile1
   mapM_ assertRow d
   where
     assertRow r = v3 @=? (v1 + v2)
@@ -48,12 +52,15 @@ test_simpleParse = do
             v2 = readBS $ r ! "Col3"
             v3 = readBS $ r ! "Sum"
 
+
 csvSettings :: CSVSettings
 csvSettings = defCSVSettings { csvQuoteChar = Just '`'
                              , csvOutputQuoteChar = Just '`' }
 
-testFile :: FilePath
-testFile = "test/test.csv"
+testFile1, testFile2 :: FilePath
+testFile1 = "test/test.csv"
+testFile2 = "test/test.csv"
+
 
 readBS :: B.ByteString -> Int
 readBS = read . B.unpack
